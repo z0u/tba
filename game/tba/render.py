8c252ec38ae93d7361a344599a1faa1bc40a6d9a
Alex Fraser <alex@phatcore.com>
2014-01-24 21:34:46 +1100

    Added articles (the, an, a).
    Also shifted rendering functions into a class so they can have
    state.

--- game/tba/render.py
+++ game/tba/render.py
@@ -8,24 +8,52 @@ def p(ob, prop, default=None):
     except KeyError:
         return default
 
-def importance_key(ob):
-    return p(ob, 'size', 1.0) * p(ob, 'rel_size', 1.0)
+class Renderer:
+    def __init__(self):
+        self.recent_obs = {}
 
-def describe_scene(c):
-    sce = bge.logic.getCurrentScene()
-    obs = list(sce.objects)
-    obs.sort(key=importance_key, reverse=True)
+    def mention(self, ob):
+        self.recent_obs[ob.name] = ob
 
-    you = sce.objects['You']
-    hitob, _, _ = you.rayCast(
-        you.worldPosition - mathutils.Vector((0, 0, 100)),
-        you.worldPosition,
-        100)
+    def is_definite_article(self, ob):
+        return ob.name in self.recent_obs and self.recent_obs[ob.name] is ob
 
-    if hitob is not None:
-        print('{sub} are standing on a {ob}'.format(sub=you.name, ob=hitob.name))
+    def article(self, ob):
+        if self.is_definite_article(ob):
+            # Object has been mentioned recently.
+            return 'the'
+        else:
+            if ob.name[0] in 'aeio':
+                return 'an'
+            else:
+                return 'a'
 
-    for ob in sce.objects:
-        if ob is you:
-            continue
-        print('There is a {ob}'.format(ob=ob.name))
+    def importance_key(self, ob):
+        return p(ob, 'size', 1.0) * p(ob, 'rel_size', 1.0)
+
+    def describe_scene(self):
+        sce = bge.logic.getCurrentScene()
+        obs = list(sce.objects)
+        obs.sort(key=self.importance_key, reverse=True)
+
+        you = sce.objects['You']
+        hitob, _, _ = you.rayCast(
+            you.worldPosition - mathutils.Vector((0, 0, 100)),
+            you.worldPosition,
+            100)
+
+        if hitob is not None:
+            print('{sub} are standing on {a} {ob}'.format(
+                sub=you.name, a=self.article(hitob), ob=hitob.name))
+            self.recent_obs[hitob.name] = hitob
+
+        for ob in sce.objects:
+            if ob is you:
+                continue
+            print('There is {a} {ob}'.format(
+                a=self.article(ob), ob=ob.name))
+            self.recent_obs[ob.name] = ob
+
+def render(c):
+    r = Renderer()
+    r.describe_scene()
